e18c5de27bd23b99befb9f42c7afdf241faa3042
Eddy Chase <eddykanyz.ek@gmail.com>
2020-12-15 17:38:09 +0300

    New Pitch app

--- manage.py
+++ manage.py
@@ -15,6 +15,6 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Pitch = Pitch )
+    return dict(app = app,db = db,User = User, Pitch = Pitches )
 if __name__ == '__main__':
     manager.run()
